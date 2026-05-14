22bc29847c7e47ea3874d526ffe2c1a3ed4e8f82
Jared Collins <collinsjared73+github@gmail.com>
2026-05-14 10:39:12 -0400

    updated firebase.js and node_modules firebase

--- user-data/firebase.js
+++ user-data/firebase.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyBM5-cQema1PzjpTPtCxIdmvTrWH2p1nc8",
-  authDomain: "vinaysomawat-portfolio.firebaseapp.com",
-  projectId: "vinaysomawat-portfolio",
-  storageBucket: "vinaysomawat-portfolio.firebasestorage.app",
-  messagingSenderId: "996992279996",
-  appId: "1:996992279996:web:f296555eef216d501337b9",
-  measurementId: "G-8PVX0MLGSK",
+  apiKey: "AIzaSyCkPsnzRq9urL9figTzNhAg3Of3t3wfmzI",
+  authDomain: "meredith-portfolio.firebaseapp.com",
+  projectId: "meredith-portfolio",
+  storageBucket: "meredith-portfolio.firebasestorage.app",
+  messagingSenderId: "144555509933",
+  appId: "1:144555509933:web:ff6e0810a48cc586531e24",
+  measurementId: "G-MKV237CP19"
 };
